f8a0efae3780a490b58f9544c9d5d6c69d2fd13f
asmomin6 <asmomin6@SL-IT-TECH1.cougarnet.uh.edu>
2019-08-03 13:22:25 -0500

    Alishah Momin changed + 2 to + 4

--- src/random_floors/Random_floors.java
+++ src/random_floors/Random_floors.java
@@ -43,7 +43,7 @@ System.out.println("--------------------------------------------------------");
 
 final Set<Integer> s = new HashSet<>();
 final Set<Integer> t = new HashSet<>();
-for(int i = 0; i < techs.length + 2; i++){
+for(int i = 0; i < techs.length + 4; i++){
                 while(true){
                 int tec = r.nextInt(techs.length) + 1;
                 do{
